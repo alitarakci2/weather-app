64c4b33201b5d866d74ad47b6a21148b66254c02
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-01-18 16:01:11 +0300

    weather api and axios implement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import { usePosition } from 'use-position';
+import axios from 'axios';
+
 
 function App() {
+
+
+  const {weather,setWeather}=useState();
+  const {latitude,longitude}=usePosition();
+//console.log(latitude);
+//console.log(longitude);
+
+const getWeatherData=async(lat,lon)=>{
+const key = process.env.REACT_APP_WEATHER_DATA;
+//onsole.log(key);
+
+try{
+  const data=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+  console.log(data)
+
+}
+catch{
+  console.log(alert);
+  alert("veriler cekilemedi");
+}
+}
+useEffect(() =>{
+
+  latitude && longitude && getWeatherData(latitude,longitude)
+
+
+},[latitude,longitude])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
